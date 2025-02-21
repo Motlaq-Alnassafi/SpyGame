7d05c5e0c24d3330713460a2e62e1102a9e698c1
Motlaq-Alnassafi <alnassafimotlaq@gmail.com>
2025-02-21 10:18:30 +0300

    Initial Commit

---
+++ Who is the spy/Who_is_the_spyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Who_is_the_spyApp.swift
+//  Who is the spy
+//
+//  Created by Motlaq Alnassafi on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Who_is_the_spyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
